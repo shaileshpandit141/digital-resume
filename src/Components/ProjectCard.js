c89a37c56e2a947a7090c545f0904fe68b58896c
shaileshpandit141 <shaileshpandit141@gmail.com>
2024-01-08 16:54:49 +0530

    Update ProjectCard and SkillData

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -4,8 +4,8 @@ export default function ProjectCard(data) {
         <div class="touch--overflow">
             <a class="project--card--link" href="https://github.com/shaileshpandit141/${data.githubLink}">
                 <h3 class="title--text">${data.title}</h3>
-                <p class='subtitle--text'>${data.subTitle}</p>
                 <p class='subtitle--text'>${data.date}</p>
+                <p class='subtitle--text'>${data.subTitle}</p>
             </a>
         </div>
         `
--- src/Data/SkillData.js
+++ src/Data/SkillData.js
@@ -7,7 +7,7 @@ export default [
     {
         id: 2,
         skillName: 'Object-Oriented Programming (OOP)',
-        rating: 5,
+        rating: 4,
     },
     {
         id: 3,
@@ -36,16 +36,21 @@ export default [
     },
     {
         id: 8,
+        skillName: 'Django Rest Framework',
+        rating: 4,
+    },
+    {
+        id: 9,
         skillName: 'ReactJS',
         rating: 3,
     },
     {
-        id: 9,
+        id: 10,
         skillName: 'PostgreSQL',
         rating: 3,
     },
     {
-        id: 10,
+        id: 11,
         skillName: 'Git',
         rating: 4,
     },
