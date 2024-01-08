15533a22587385aeb195638274aac863031fca8c
shaileshpandit141 <shaileshpandit141@gmail.com>
2024-01-08 23:18:43 +0530

    update loader styles

--- src/App.js
+++ src/App.js
@@ -34,20 +34,3 @@ const certificatecontEl = document.getElementById('certificate--cont')
 CertificateData.forEach(data => {
     certificatecontEl.insertAdjacentHTML('beforeend', CertificateCard(data))
 })
-
-
-// || Hendel Loder Element
-const loderEl = document.getElementById('loder')
-window.addEventListener('load', event => {
-    event.preventDefault()
-
-    // || Adding css Class to hide loader
-    loderEl.classList.add('loder--hidden')
-
-    loderEl.addEventListener('transitionend', event => {
-        event.preventDefault()
-
-        // || For Deleting element from the document 
-        document.body.removeChild(loderEl)
-    })
-})
---
+++ src/loader.js
@@ -0,0 +1,15 @@
+// || Hendel Loder Element
+const loaderEl = document.getElementById('loader')
+window.addEventListener('load', event => {
+    event.preventDefault()
+
+    // || Adding css Class to hide loader
+    loaderEl.classList.add('loader--hidden')
+
+    loaderEl.addEventListener('transitionend', event => {
+        event.preventDefault()
+
+        // || For Deleting element from the document 
+        document.body.removeChild(loaderEl)
+    })
+})
