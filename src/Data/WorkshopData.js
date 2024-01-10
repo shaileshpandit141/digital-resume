f78fd62de58d4d2b7c760806cb2d95d992f3af69
SHAILESH pandit <111145844+shaileshpandit141@users.noreply.github.com>
2024-01-10 23:39:37 +0530

    Update WorkshopData.js

--- src/Data/WorkshopData.js
+++ src/Data/WorkshopData.js
@@ -1,14 +1,14 @@
 export default [
     {
         id: 1,
-        title: 'Machine Learning Workshop with Python',
-        date: '11/2023 - 12/2023',
-        services: 'Mysirg Education Services Pvt Ltd',
-    },
-    {
-        id: 2,
         title: 'Arduino Programming',
         date: '10/2021 - 11/2021',
         services: 'VIT Bhopal University',
+    }, 
+    {
+        id: 2,
+        title: 'Machine Learning Workshop with Python',
+        date: '11/2023 - 12/2023',
+        services: 'Mysirg Education Services Pvt Ltd',
     },
 ]
