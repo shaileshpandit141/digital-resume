80eb8954a5c5f2bdad6203a188434143c9b79479
SHAILESH pandit <111145844+shaileshpandit141@users.noreply.github.com>
2024-01-10 23:38:13 +0530

    Update CertificateData.js

--- src/Data/CertificateData.js
+++ src/Data/CertificateData.js
@@ -1,26 +1,27 @@
 export default [
     {
         id: 1,
-        title: 'Full Stack Web Development using Python',
-        date: '05/2022 - 11/2022',
-        services: 'ineuron.ai',
+        title: 'Arduino Programming',
+        date: '10/2021 - 11/2021',
+        services: 'VIT Bhopal University',
     },
     {
         id: 2,
         title: 'Python for beginners',
         date: '11/2022 - 11/2022',
         services: 'kodacy.com',
-    },
+    }, 
     {
         id: 3,
+        title: 'Full Stack Web Development using Python',
+        date: '05/2022 - 11/2022',
+        services: 'ineuron.ai',
+    },
+    {
+        id: 4,
         title: 'Machine Learning Workshop with Python',
         date: '11/2023 - 12/2023',
         services: 'Mysirg Education Services Pvt Ltd',
     },
-    {
-        id: 4,
-        title: 'Arduino Programming',
-        date: '10/2021 - 11/2021',
-        services: 'VIT Bhopal University',
-    },
+    
 ]
